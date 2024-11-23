2af7bc32fcebc62b9a024a0b1cac03d6c4f37529
Peterb1700 <peterbruno710@gmail.com>
2024-11-23 12:54:55 -0500

    Update retail_sales_query.sql
    Cleaned up code

--- retail_sales_query.sql
+++ retail_sales_query.sql
@@ -20,7 +20,6 @@ CREATE TABLE retail_sales
 			);
 			
 ----Data cleaning 
-SELECT * from retail_sales 
 
 ---Deleting all null values 
 
@@ -50,8 +49,7 @@ FROM retail_sales--How many customers we have
 SELECT * FROM retail_sales
 WHERE sale_date = '2022-11-05'
 
----Write a SQL query to retrieve all transactions where the category is 
---'Clothing' and the quantity sold is more than 4 in the month of Nov-2022:
+---Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022:
 
 SELECT 
   *
@@ -72,22 +70,20 @@ SELECT
 FROM retail_sales
 GROUP BY 1
 
---Write a SQL query to find the average age of customers who 
---purchased items from the 'Beauty' category:
+--Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category:
 
 SELECT
     ROUND(AVG(age), 2) as avg_age
 FROM retail_sales
 WHERE category = 'Beauty'
 
---Write a SQL query to find all transactions where the total_sale is greater than 1000.:
+--Write a SQL query to find all transactions where the total_sale is greater than 1000:
 
 SELECT * FROM retail_sales
 WHERE total_sale > 1000
 
 
----Write a SQL query to find the total number of transactions (transaction_id) 
----made by each gender in each category.:
+---Write a SQL query to find the total number of transactions made by each gender in each category.:
 
 SELECT 
     category,
@@ -117,7 +113,7 @@ GROUP BY 1, 2
 ) as t1
 WHERE rank = 1
 
----**Write a SQL query to find the top 5 customers based on the highest total sales **:
+---Write a SQL query to find the top 5 customers based on the highest total sales:
 
 SELECT customer_id, sum(total_sale) as total_sales
 FROM retail_sales
@@ -126,7 +122,7 @@ ORDER BY total_sales DESC LIMIT 5
 
 
 ---Write a SQL query to find the number of unique customers who 
---purchased items from each category.:
+--purchased items from each category:
 
 SELECT 
     category,    
@@ -147,7 +143,7 @@ SELECT *,
     END as shift
 FROM retail_sales
 )
-SELECT 
+ ---end project
     shift,
     COUNT(*) as total_orders    
 FROM hourly_sale
